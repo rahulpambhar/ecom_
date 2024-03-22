76370000dde341c3ede17acf36391dfc4dd38c22
Rahul KoffeKodes <rahulpambhar.koffeekodes@gmail.com>
2024-03-23 02:44:44 +0530

    landing page implemented

--- src/components/frontside/ButtonCustom.js
+++
@@ -1,9 +0,0 @@
-function ButtonCustom({ name = "Button" }) {
-  return (
-    <div className="bg-[var(--primary2)] cursor-pointer p-2 rounded-lg text-[var(--secondary5)] max-w-[160px] text-center font-[500] hover:shadow-sm hover:shadow-[--primary1]">
-      {name}
-    </div>
-  );
-}
-
-export default ButtonCustom;
--- src/components/frontside/filter/Filter.tsx
+++ src/components/frontside/filter/Filter.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { Categories, SubCategory } from "../../../../types/global";
+import { useAppSelector, useAppDispatch } from '../../../app/redux/hooks';
+import { fetchCategories } from "@/app/redux/slices/categorySlice";
+import { useParams } from "next/navigation";
 
 const Filter = () => {
+  const params = useParams();
+  const dispatch = useAppDispatch();
+  const categories: Categories[] = useAppSelector((state): any => state?.categories?.categories);
+  const subCategories: SubCategory[] = categories.filter((item: any) => item?.name === params.sub).flatMap((category: any) => category.SubCategory);
+
+  useEffect(() => {
+    dispatch(fetchCategories({ page: 1, limit: 100 }));
+  }, [dispatch]);
+  
   return (
     <div className="flex  justify-between items-center border-b-2 ">
       <div className="flex justify-center items-center">
         <div className="flex justify-between w-[322px] border-r-2 py-[22px]">
           <div className="text-xl font-bold poppins uppercase pl-16">
-            FILTERS :
+            sub category :
           </div>
           <div className="flex justify-center items-center text-sm pr-6 font-light poppins uppercase">
-            CLEAR ALL
+            <select className="outline-none">
+              {subCategories.map(sub => (
+                <option key={sub.id} value={sub.name}>{sub.name}</option>
+              ))}
+            </select>
+            {/* CLEAR ALL */}
           </div>
         </div>
         <div className="flex gap-5 pl-5">
